6fb668586cbbe8a26ff32e8619d8a7566de60c7b
Jan Kryšpín <crishpeen@gmail.com>
2017-05-29 15:31:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# stylelint-config-visionapps-order
+
+[![Build Status](https://travis-ci.org/visionappscz/stylelint-config-visionapps-order.svg?branch=master)](https://travis-ci.org/visionappscz/stylelint-config-visionapps-order)
+[![dependency Status](https://david-dm.org/visionappscz/stylelint-config-visionapps-order/status.svg)](https://david-dm.org/visionappscz/stylelint-config-visionapps-order)
+[![peerDependency Status](https://david-dm.org/visionappscz/stylelint-config-visionapps-order/peer-status.svg)](https://david-dm.org/visionappscz/stylelint-config-visionapps-order?type=peer)
+[![devDependency Status](https://david-dm.org/visionappscz/stylelint-config-visionapps-order/dev-status.svg)](https://david-dm.org/visionappscz/stylelint-config-visionapps-order?type=dev)
+
+VisionApps' shareable config of properties order for [stylelint](https://github.com/stylelint/stylelint).
+
+Lint [properties order](./index.js) with [stylelint-order](https://github.com/hudochenkov/stylelint-order).
---
+++ __tests__/index.js
@@ -0,0 +1,62 @@
+const config = require('../');
+const stylelint = require('stylelint');
+
+const validCss = (`
+.selector::before {
+    content: '';
+    position: relative;    
+}
+`);
+
+const invalidCss = (`
+.selector::before {
+    position: relative;    
+    content: '';
+}
+`);
+
+describe('flags no warnings with valid CSS', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validCss,
+      config,
+    });
+  });
+
+  it('did not error', () => (
+    result.then(data => (
+      expect(data.errored).toBeFalsy()
+    ))
+  ));
+
+  it('flags no warnings', () => (
+    result.then(data => (
+      expect(data.results[0].warnings.length).toBe(0)
+    ))
+  ));
+});
+
+describe('flags warnings with invalid CSS', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: invalidCss,
+      config,
+    });
+  });
+
+  it('did error', () => (
+    result.then(data => (
+      expect(data.errored).toBeTruthy()
+    ))
+  ));
+
+  it('flags one warning', () => (
+    result.then(data => (
+      expect(data.results[0].warnings.length).toBe(1)
+    ))
+  ));
+});
